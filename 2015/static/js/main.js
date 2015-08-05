ffe1c989d803a82003ab066f15f6a2c3742a2906
Patrick Mazulo <pmazulo@gmail.com>
2015-08-05 12:54:43 -0300

    Added speakers, logo indacode and modified style

--- 2015/static/js/main.js
+++ 2015/static/js/main.js
@@ -58,3 +58,7 @@ $(function() {
 // 		}
 // 	}).init();
 // });
+
+
+
+function init_map(){var myOptions = {zoom:16,center:new google.maps.LatLng(-2.8996462955410185,-41.72429728487549),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(-2.8996462955410185, -41.72429728487549)});infowindow = new google.maps.InfoWindow({content:"<b>Faculdade Maur&iacute;cio de Nassau</b><br/>Avenida Leonardo de Carvalho<br/> Parna&iacute;ba - PI" });google.maps.event.addListener(marker, "click", function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);
